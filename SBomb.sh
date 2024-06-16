58b1c0173999b9551e5d5bc9162bcf1368e2773f
Sabiralidyd786 <sabiralidyd786@gmail.com>
2024-06-16 15:26:37 +0530

    SBomb.sh

--- SBomb.sh
+++ SBomb.sh
@@ -32,7 +32,7 @@ banner() {
         figlet SBomb
     fi
     if ! [ -x "$(command -v toilet)" ]; then
-        echo -e "\e[4;34m This Bomber Was Created By \e[1;32mSpeedX \e[0m"
+        echo -e "\e[4;34m This Bomber Was Created By \e[1;32mNURAIN \e[0m"
     else
         echo -e "\e[1;34mCreated By \e[1;34m"
         toilet -f mono12 -F border NURAIN
